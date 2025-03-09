e14b988f5e53d756959cf5ece2f8adc88ec810cd
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2025-03-09 13:31:17 -0300

    feat: adicionando os DTOs de produtos

---
+++ CaseChatGPT.App/DTOs/Produto/CriarProdutoDTO.cs
@@ -0,0 +1,18 @@
+﻿using CaseChatGPT.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseChatGPT.App.DTOs.Produto
+{
+    public class CriarProdutoDTO
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
---
+++ CaseChatGPT.App/DTOs/Produto/ObterProdutosDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseChatGPT.App.DTOs.Produto
+{
+    public class ObterProdutosDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+}
