296cf6a6a81684f1fbb3f1fa0ee39cf8fda340ad
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2025-03-09 21:26:38 -0300

    refact: remoção código inútil

--- CaseChatGPT.Domain/Interfaces/UseCases/IProdutoUseCases.cs
+++ CaseChatGPT.Domain/Interfaces/UseCases/IProdutoUseCases.cs
@@ -1,9 +1,4 @@
 ﻿using CaseChatGPT.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CaseChatGPT.Domain.Interfaces.UseCases
 {
