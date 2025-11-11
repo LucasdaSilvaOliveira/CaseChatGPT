0fc31099aa27f04aeefa9e29c5c7a8dc8b4e0ffd
Lucas da Silva Oliveira <107443575+LucasdaSilvaOliveira@users.noreply.github.com>
2025-11-11 20:04:38 -0300

    feat: RoleRepository implementation

---
+++ CaseChatGPT.Domain/Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using CaseChatGPT.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseChatGPT.Domain.Interfaces.Repositories
+{
+    public interface IRoleRepository
+    {
+        Task<IEnumerable<Role>> GetAllRoles();
+        Task<Role> GetRoleById(string id);
+        Task UpdateRole(Role role);
+        Task DeleteRole(Role role);
+        Task<Role> GetRoleByUserAsync(string userId);
+    }
+}
---
+++ CaseChatGPT.Infra/Repositories/RoleRepository.cs
@@ -0,0 +1,47 @@
+﻿using CaseChatGPT.Domain.Entities;
+using CaseChatGPT.Domain.Interfaces.Repositories;
+using CaseChatGPT.Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseChatGPT.Infra.Repositories
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly BancoContext _bancoContext;
+        public RoleRepository(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+
+        public async Task<IEnumerable<Role>> GetAllRoles()
+        {
+            var roles = await _bancoContext.Roles.ToListAsync();
+
+            return roles;
+        }
+
+        public async Task<Role> GetRoleById(string id)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task DeleteRole(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateRole(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Role> GetRoleByUserAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
